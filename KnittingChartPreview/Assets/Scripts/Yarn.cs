481d0bd0f8a042c9da42cf81de22e7ebd78c1342
Laura Kasian <laura.kasian@gmail.com>
2024-02-10 14:05:45 -0800

    update to Yarn scirpt

--- KnittingChartPreview/Assets/Scripts/Yarn.cs
+++ KnittingChartPreview/Assets/Scripts/Yarn.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class CreateCylinder : MonoBehaviour
 {
-    public int numSegments = 30;
+    [FormerlySerializedAs("numSegments")] public int numRadialSegments = 300;
+    public int numSegments = 2;
     public float radius = 1f;
-    public float height = 2f;
+    public float height = 20f;
 
     void Start()
     {
         CreateCylinderObject();
     }
 
+    float GetVerticalOffset(int row, int stitch)
+    {
+        return 0.0f;
+    }
+    
+    float GetDepthOffset(int row, int stitch)
+    {
+        return 0.0f;
+    }
+    
     void CreateCylinderObject()
     {
         GameObject cylinder = new GameObject("Cylinder");
@@ -19,34 +31,37 @@ public class CreateCylinder : MonoBehaviour
         Mesh mesh = new Mesh();
         meshFilter.mesh = mesh;
 
-        Vector3[] vertices = new Vector3[numSegments * 2];
-        int[] triangles = new int[numSegments * 6];
+        Vector3[] vertices = new Vector3[numRadialSegments * 2];
+        int[] triangles = new int[numRadialSegments * 6];
 
         // Generate vertices
-        for (int i = 0; i < numSegments; i++)
+        for (int i = 0; i < numRadialSegments; i++)
         {
-            float angle = Mathf.PI * 2 * i / numSegments;
-            float x = height / 2;
-            float y = Mathf.Cos(angle) * radius;
-            float z = Mathf.Sin(angle) * radius;
-            Debug.Log($"{x} {y} {z}");
-            vertices[i] = new Vector3(x, y, z);
-            vertices[i + numSegments] = new Vector3(-1.0f * x, y, z);
+            float angle = Mathf.PI * 2 * i / numRadialSegments;
+            float y = Mathf.Cos(angle) * radius + GetVerticalOffset(0, 0);
+            float z = Mathf.Sin(angle) * radius + GetDepthOffset(0, 0);
+            // Debug.Log($"{x} {y} {z}");
+            for (int j = 0; j < numSegments; j++)
+            {
+                float x = height / 2 - (j + 1) / (numSegments + 1) * height;
+                vertices[i + j * numRadialSegments] = new Vector3(x, y, z);
+                Debug.Log($"i, j, x: {i}, {j}, {x}");
+            }
         }
 
         // Generate triangles
-        for (int i = 0; i < numSegments; i++)
+        for (int i = 0; i < numRadialSegments; i++)
         {
-            int nextIndex = (i + 1) % numSegments;
+            int nextIndex = (i + 1) % numRadialSegments;
 
             // Side triangles
             triangles[i * 6] = i;
-            triangles[i * 6 + 1] = i + numSegments;
+            triangles[i * 6 + 1] = i + numRadialSegments;
             triangles[i * 6 + 2] = nextIndex;
 
             triangles[i * 6 + 3] = nextIndex;
-            triangles[i * 6 + 4] = i + numSegments;
-            triangles[i * 6 + 5] = nextIndex + numSegments;
+            triangles[i * 6 + 4] = i + numRadialSegments;
+            triangles[i * 6 + 5] = nextIndex + numRadialSegments;
         }
 
         mesh.vertices = vertices;
