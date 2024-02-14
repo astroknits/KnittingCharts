b8e7c76640ad8893f75d3375b94ad66513c8ed13
Laura Kasian <laura.kasian@gmail.com>
2024-02-13 17:18:59 -0800

    refactor

--- KnittingChartPreview/Assets/Scripts/Editor/YarnEditor.cs
+++ KnittingChartPreview/Assets/Scripts/Editor/YarnEditor.cs
@@ -2,42 +2,47 @@ using UnityEditor;
 using UnityEngine;
 using YarnGenerator;
 
-public class YarnEditor : EditorWindow
+namespace YarnGenerator
 {
-    // Parameters for yarn
-    float rowLength = 2f;
-    float stitchLength = 2f;
-    float yarnWidth = 0.1f;
-    
-    // Add menu item named "My Window" to the Window menu
-    [MenuItem("Window/Create Yarn")]
-    public static void ShowWindow()
+    public class YarnEditor : EditorWindow
     {
-        //Show existing window instance. If one doesn't exist, make one.
-        EditorWindow.GetWindow(typeof(YarnEditor));
-    }
-    
-    void OnGUI()
-    {
-        GUILayout.Label ("Yarn Settings", EditorStyles.boldLabel);
-        
-        rowLength = EditorGUILayout.Slider(
-            "Row Length", rowLength, 1f, 300f);
-        stitchLength = EditorGUILayout.Slider(
-            "Stitch Length", stitchLength, 0.5f, 5f);
-        yarnWidth = EditorGUILayout.Slider(
-            "Width", yarnWidth, 0.0001f, 0.5f);
+        // Parameters for yarn
+        float rowLength = 2f;
+        float stitchLength = 2f;
+        float yarnWidth = 0.1f;
 
-
-        if (GUILayout.Button("Generate Yarn"))
+        // Add menu item named "My Window" to the Window menu
+        [MenuItem("Window/Create Yarn")]
+        public static void ShowWindow()
         {
-            if (yarnWidth > stitchLength / 8.0f)
-            {
-                Debug.LogError("Yarn Width needs to be less than 1/8 the stitch length"
-                + $"Please choose a yarn width less than {stitchLength/8.0f}");
-                return;}
-            Yarn.GenerateRow(KnitSettings.radialRes, KnitSettings.stitchRes, yarnWidth, stitchLength);
+            //Show existing window instance. If one doesn't exist, make one.
+            EditorWindow.GetWindow(typeof(YarnEditor));
         }
 
+        void OnGUI()
+        {
+            GUILayout.Label("Yarn Settings", EditorStyles.boldLabel);
+
+            rowLength = EditorGUILayout.Slider(
+                "Row Length", rowLength, 1f, 300f);
+            stitchLength = EditorGUILayout.Slider(
+                "Stitch Length", stitchLength, 0.5f, 5f);
+            yarnWidth = EditorGUILayout.Slider(
+                "Width", yarnWidth, 0.0001f, 0.5f);
+
+
+            if (GUILayout.Button("Generate Yarn"))
+            {
+                if (yarnWidth > stitchLength / 8.0f)
+                {
+                    Debug.LogError("Yarn Width needs to be less than 1/8 the stitch length"
+                                   + $"Please choose a yarn width less than {stitchLength / 8.0f}");
+                    return;
+                }
+
+                Yarn.GenerateRow(KnitSettings.radialRes, KnitSettings.stitchRes, yarnWidth, stitchLength);
+            }
+
+        }
     }
 }
--- KnittingChartPreview/Assets/Scripts/Yarn.cs
+++ KnittingChartPreview/Assets/Scripts/Yarn.cs
@@ -3,156 +3,174 @@ using UnityEngine;
 using UnityEngine.Serialization;
 using YarnGenerator;
 
-public class Yarn : MonoBehaviour
+namespace YarnGenerator
 {
-    public float stitchLength = 1f;
-    public float yarnWidth = 0.1f;
-
-    public static void GenerateRow(int radialRes, int stitchRes, float yarnWidth, float stitchLength)
+    public class Yarn : MonoBehaviour
     {
-        GameObject yarn = new GameObject("Yarn");
-        MeshFilter meshFilter = yarn.AddComponent<MeshFilter>();
-        MeshRenderer meshRenderer = yarn.AddComponent<MeshRenderer>();
-        Mesh mesh = new Mesh();
-        meshFilter.mesh = mesh;
+        public float stitchLength = 1f;
+        public float yarnWidth = 0.1f;
 
-        // Generate curve
-        Vector3[] curve = GenerateCurve(stitchLength, stitchRes);
-        
-        mesh.vertices = GenerateVertices(curve, radialRes, stitchRes, yarnWidth, stitchLength);
-        mesh.triangles = GenerateTriangles(radialRes, stitchRes);
-        mesh.RecalculateNormals();
-
-        // Assign a default material
-        meshRenderer.material = new Material(Shader.Find("Standard"));
-    }
-
-    static float sigmoid(float x)
-    {
-        // sigmoid(x) = 1 / (1 + exp(-x))
-        return 1.0f / (1.0f + (float) Math.Exp(-1.0f * x));
-    }
-
-    static float GetVerticalOffsetForStitch(int index, int stitchRes)
-    {
-        float kappa = 0.25f;
-        float scale = 4.0f; // range goes from -scale to +scale
-        float shift = 0.8f;
-        float pos = scale * (2 * (float) index / (float) stitchRes - 0.5f);
-
-        if (index * 2 > stitchRes)
+        public static void GenerateRow(int radialRes, int stitchRes, float yarnWidth, float stitchLength)
         {
-            pos = scale - (pos + shift);
-        }
-        else
-        {
-            pos = pos - shift;
-        }
-        
-        float res = sigmoid(pos / kappa);
-        return res;
-    }
+            // For now, each row only has one stitch in it
 
-    static float GetVerticalOffset(int index, int stitchRes)
-    {
-        return GetVerticalOffsetForStitch(index, stitchRes);
-    }
-    
-    static float GetDepthOffset(int index)
-    {
-        return 0.0f;
-    }
+            // Create the mesh for the yarn in this row
+            GameObject yarn = new GameObject("Yarn");
+            MeshFilter meshFilter = yarn.AddComponent<MeshFilter>();
+            MeshRenderer meshRenderer = yarn.AddComponent<MeshRenderer>();
+            Mesh mesh = new Mesh();
+            meshFilter.mesh = mesh;
 
-    static Vector3[] GenerateCurve(float length, int stitchRes)
-    {
-        Vector3[] curve = new Vector3[stitchRes + 1];
-        for (int j = 0; j < stitchRes + 1; j++)
-        {
-            float x = (float)j / (float)stitchRes - 0.5f;
-            float verticalOffset = GetVerticalOffset(j, stitchRes);
-            float depthOffset = GetDepthOffset(j);
-            curve[j] = new Vector3(
-                length * x,  verticalOffset, depthOffset);
-            // Debug.Log($"j, x, y: {j} {curve[j].x} {curve[j].y}");
-        }
-        return curve;
-    }
+            // Stitch stitch = new Stitch(yarnWidth, stitchLength);
+            // stitch.GenerateCurve();
 
-    static Vector3[] GenerateCircle(float yarnWidth, int radialRes)
-    {
-        Vector3[] circle = new Vector3[radialRes];
-        for (int i = 0; i < radialRes; i++)
-        {
-            float angle = Mathf.PI * 2 * i / radialRes;
-            circle[i] = new Vector3(
-                0.0f, yarnWidth * Mathf.Cos(angle), yarnWidth * Mathf.Sin(angle));
-        }
-        return circle;
-    }
+            // Generate curve for the stitch
+            Vector3[] curve = GenerateCurve(stitchLength, stitchRes);
 
-    static Vector3[] GenerateRotatedCircle(Vector3[] circle, Vector3[] curve, int radialRes, int stitchRes, int j)
-    {
-        Vector3[] rotatedCircle = new Vector3[radialRes];
-        float theta = 0.0f;
+            // Set up vertices and triangles for the row based on the curve
+            Vector3[] vertices = GenerateVertices(curve, radialRes, stitchRes, yarnWidth, stitchLength);
+            int[] triangles = GenerateTriangles(radialRes, stitchRes);
 
-        if (j < stitchRes)
-        {
-            Vector3 diff = curve[j + 1] - curve[j];
-            float length = (float)(Math.Sqrt(Math.Pow(diff.x, 2) + Math.Pow(diff.y, 2)));
-            theta = (float) Math.Asin(diff.y / length);
+            mesh.vertices = vertices;
+            mesh.triangles = triangles;
+            mesh.RecalculateNormals();
+
+            // Assign a default material
+            meshRenderer.material = new Material(Shader.Find("Standard"));
         }
 
-        for (int i = 0; i < radialRes; i++)
+        static float sigmoid(float x)
         {
-            rotatedCircle[i] = new Vector3(
-                curve[j].x + circle[i].y * (float) Math.Sin(theta),
-                curve[j].y - circle[i].y * (float) Math.Cos(theta),
-                curve[j].z + circle[i].z
-            );
+            // sigmoid(x) = 1 / (1 + exp(-x))
+            return 1.0f / (1.0f + (float) Math.Exp(-1.0f * x));
         }
 
-        return rotatedCircle;
-    }
-    static Vector3[] GenerateVertices(Vector3[] curve, int radialRes, int stitchRes, float yarnWidth, float stitchLength)
-    {
-        // Generate vertices
-        Vector3[] circle = GenerateCircle(yarnWidth, radialRes);
-        Vector3[] vertices = new Vector3[radialRes * (stitchRes + 1)];
-        for (int j = 0; j < stitchRes + 1; j++)
+        static float GetVerticalOffsetForStitch(int index, int stitchRes)
         {
+            float kappa = 0.25f;
+            float scale = 4.0f; // range goes from -scale to +scale
+            float shift = 0.8f;
+            float pos = scale * (2 * (float) index / (float) stitchRes - 0.5f);
+
+            if (index * 2 > stitchRes)
+            {
+                pos = scale - (pos + shift);
+            }
+            else
+            {
+                pos = pos - shift;
+            }
+
+            float res = sigmoid(pos / kappa);
+            return res;
+        }
+
+        static float GetVerticalOffset(int index, int stitchRes)
+        {
+            return GetVerticalOffsetForStitch(index, stitchRes);
+        }
+
+        static float GetDepthOffset(int index)
+        {
+            return 0.0f;
+        }
+
+        static Vector3[] GenerateCurve(float length, int stitchRes)
+        {
+            Vector3[] curve = new Vector3[stitchRes + 1];
+            for (int j = 0; j < stitchRes + 1; j++)
+            {
+                float x = (float) j / (float) stitchRes - 0.5f;
+                float verticalOffset = GetVerticalOffset(j, stitchRes);
+                float depthOffset = GetDepthOffset(j);
+                curve[j] = new Vector3(
+                    length * x, verticalOffset, depthOffset);
+                // Debug.Log($"j, x, y: {j} {curve[j].x} {curve[j].y}");
+            }
+
+            return curve;
+        }
+
+        static Vector3[] GenerateCircle(float yarnWidth, int radialRes)
+        {
+            Vector3[] circle = new Vector3[radialRes];
+            for (int i = 0; i < radialRes; i++)
+            {
+                float angle = Mathf.PI * 2 * i / radialRes;
+                circle[i] = new Vector3(
+                    0.0f, yarnWidth * Mathf.Cos(angle), yarnWidth * Mathf.Sin(angle));
+            }
+
+            return circle;
+        }
+
+        static Vector3[] GenerateRotatedCircle(Vector3[] circle, Vector3[] curve, int radialRes, int stitchRes, int j)
+        {
+            Vector3[] rotatedCircle = new Vector3[radialRes];
+            float theta = 0.0f;
+
+            if (j < stitchRes)
+            {
+                Vector3 diff = curve[j + 1] - curve[j];
+                float length = (float) (Math.Sqrt(Math.Pow(diff.x, 2) + Math.Pow(diff.y, 2)));
+                theta = (float) Math.Asin(diff.y / length);
+            }
+
+            for (int i = 0; i < radialRes; i++)
+            {
+                rotatedCircle[i] = new Vector3(
+                    curve[j].x + circle[i].y * (float) Math.Sin(theta),
+                    curve[j].y - circle[i].y * (float) Math.Cos(theta),
+                    curve[j].z + circle[i].z
+                );
+            }
+
+            return rotatedCircle;
+        }
+
+        static Vector3[] GenerateVertices(Vector3[] curve, int radialRes, int stitchRes, float yarnWidth,
+            float stitchLength)
+        {
+            // Generate vertices
+            Vector3[] circle = GenerateCircle(yarnWidth, radialRes);
+            Vector3[] vertices = new Vector3[radialRes * (stitchRes + 1)];
+            for (int j = 0; j < stitchRes + 1; j++)
+            {
                 Vector3[] rotatedCircle = GenerateRotatedCircle(circle, curve, radialRes, stitchRes, j);
                 for (int i = 0; i < radialRes; i++)
                 {
                     vertices[j * radialRes + i] = rotatedCircle[i];
                 }
-        }
-        return vertices;
-    }
-
-    static int[] GenerateTriangles(int radialRes, int stitchRes)
-    {
-        int[] triangles = new int[radialRes * stitchRes * 6];
-        
-        for (int i = 0; i < radialRes; i++)
-        {
-            // Generate triangles
-            for (int j = 0; j < stitchRes; j++)
-            {
-                int index = j * radialRes + i;
-                int nextIndex = j * radialRes + (i + 1) % radialRes;
-                int triangleIndex = i * 6 + j * radialRes * 6;
-                // Side triangles
-                triangles[triangleIndex] = nextIndex;
-                triangles[triangleIndex + 1] = index + radialRes;
-                triangles[triangleIndex + 2] = index;
-                triangles[triangleIndex + 3] = nextIndex + radialRes;
-                triangles[triangleIndex + 4] = index + radialRes;
-                triangles[triangleIndex + 5] = nextIndex;
             }
+
+            return vertices;
         }
 
-        return triangles;
+        static int[] GenerateTriangles(int radialRes, int stitchRes)
+        {
+            int[] triangles = new int[radialRes * stitchRes * 6];
+
+            for (int i = 0; i < radialRes; i++)
+            {
+                // Generate triangles
+                for (int j = 0; j < stitchRes; j++)
+                {
+                    int index = j * radialRes + i;
+                    int nextIndex = j * radialRes + (i + 1) % radialRes;
+                    int triangleIndex = i * 6 + j * radialRes * 6;
+                    // Side triangles
+                    triangles[triangleIndex] = nextIndex;
+                    triangles[triangleIndex + 1] = index + radialRes;
+                    triangles[triangleIndex + 2] = index;
+                    triangles[triangleIndex + 3] = nextIndex + radialRes;
+                    triangles[triangleIndex + 4] = index + radialRes;
+                    triangles[triangleIndex + 5] = nextIndex;
+                }
+            }
+
+            return triangles;
+        }
+
+
     }
-
-
 }
