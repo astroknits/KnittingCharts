963fa20bbd0c516c605033e32358c9ff35dd59e4
Laura Kasian <laura.kasian@gmail.com>
2024-02-20 09:38:27 -0800

    clean up GenerateCircle method

--- KnittingChartPreview/Assets/Scripts/Yarn.cs
+++ KnittingChartPreview/Assets/Scripts/Yarn.cs
@@ -39,9 +39,14 @@ namespace YarnGenerator
             return yarn;
         }
 
-        internal Vector3[] RotateCircle(float yarnWidth, Vector3[] curve, int j)
+        internal Vector3[] GenerateCircle(float yarnWidth, Vector3[] curve, int j)
         {
-            Vector3[] rotatedCircle = new Vector3[KnitSettings.radialRes];
+            /* Create circle of points in a plane normal to the direction
+             * of the curve.
+             * Start by generating a circle of points in the y-z plane, and
+             * apply rotations about the z-axis (theta) and y-axis (phi).
+             */
+            Vector3[] circle = new Vector3[KnitSettings.radialRes];
             float theta = 0.0f;
             float phi = 0.0f;
             float cosTheta = 1.0f;
@@ -49,28 +54,39 @@ namespace YarnGenerator
             float cosPhi = 1.0f;
             float sinPhi = 0.0f;
 
+            // If j >= curve.Length, theta and phi remain the default values of 0.0f
             if (j < curve.Length - 1)
             {
+                // Direction vector, acts as the normal to the circle
                 Vector3 diff = curve[j + 1] - curve[j];
+
+                // Calculate theta (angle from z-axis) and phi (angle from y-axis)
                 float length = (float) (Math.Sqrt(Math.Pow(diff.x, 2) + Math.Pow(diff.y, 2) + Math.Pow(diff.z, 2)));
                 theta = (float) Math.Asin(diff.y / length);
                 phi = (float) Math.Asin(diff.z / length / Math.Cos(theta));
+
+                // Precalculate the sine and cosine of the angles
                 cosTheta = (float)Math.Cos(theta);
                 sinTheta = (float)Math.Sin(theta);
                 cosPhi = (float)Math.Cos(phi);
                 sinPhi = (float)Math.Sin(phi);
             }
 
-            // generates circle of width yarnWidth in y-z plane
-            // for each point in the stitch curve
 
             for (int i = 0; i < KnitSettings.radialRes; i++)
             {
+                // Angle runs from 0 to 2*Pi
                 float angle = Mathf.PI * 2 * i / KnitSettings.radialRes;
+
+                // generates circle of width yarnWidth in y-z plane
+                // for each point in the stitch curve
                 float cx = 0.0f;
                 float cy = yarnWidth * Mathf.Cos(angle);
                 float cz = yarnWidth * Mathf.Sin(angle);
 
+                // Now rotate the circle so its normal is
+                // in direction of the diff vector
+                // 
                 // Calculate 3d rotations
                 // https://stackoverflow.com/questions/14607640/rotating-a-vector-in-3d-space
                 // rotate around the z axis first (theta)
@@ -81,14 +97,15 @@ namespace YarnGenerator
                 dx = dx * cosPhi + dz * sinPhi;
                 dz = -1.0f * dx * sinPhi + dz * cosPhi;
 
-                rotatedCircle[i] = new Vector3(
+                // Add the circle at the point curve[j]
+                circle[i] = new Vector3(
                     curve[j].x + dx,
                     curve[j].y - dy,
                     curve[j].z + dz
                 );
             }
 
-            return rotatedCircle;
+            return circle;
         }
 
         internal Vector3[] GenerateVerticesForRow(
@@ -129,7 +146,7 @@ namespace YarnGenerator
             ];
             for (int j = 0; j < curve.Length; j++)
             {
-                Vector3[] rotatedCircle = RotateCircle(yarnWidth, curve, j);
+                Vector3[] rotatedCircle = GenerateCircle(yarnWidth, curve, j);
                 for (int i = 0; i < KnitSettings.radialRes; i++)
                 {
                     vertices[j * KnitSettings.radialRes + i] = rotatedCircle[i];
